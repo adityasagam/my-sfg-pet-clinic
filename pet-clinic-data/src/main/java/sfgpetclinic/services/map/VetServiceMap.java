2d252127aa94ae529e2eba8a18b61344b78bde8a
adityasagam <aditya.sagam@gmail.com>
2022-06-05 00:37:10 -0700

    fix err

--- pet-clinic-data/src/main/java/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/sfgpetclinic/services/map/VetServiceMap.java
@@ -13,7 +13,7 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements Crud
 
     @Override
     public Vet findById(Long id) {
-        return super.findById(id);;
+        return super.findById(id);
     }
 
     @Override
